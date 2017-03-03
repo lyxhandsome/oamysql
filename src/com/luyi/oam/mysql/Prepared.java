9570ab0981e59c99ab640394820da0698e7e2056
lyxhandsome <lyxugly@126.com>
2017-03-03 21:27:48 +0800

    git@github.com:lyxhandsome/hello-world.git

--- src/com/luyi/oam/mysql/Prepared.java
+++ src/com/luyi/oam/mysql/Prepared.java
@@ -8,7 +8,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Prepared {
-
     public static void main(String[] args) {
         Connection con = null;
         PreparedStatement pst = null;
